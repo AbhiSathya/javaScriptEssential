a1b1925b55b647311465ede0c5deeb4467efdc25
deepak reddy <deepakreddy6525@gmail.com>
2024-02-01 20:59:27 +0530

    initial commit

---
+++ followersCount/followers_count.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } 
+    else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function decreaseCount() {
+    if(count > 0) {
+        count--;
+        displayCount();
+        checkCountValue();
+    }
+}
+
+function resetCount() {
+    count=0;
+    displayCount();
+}
