d7d14d36676f74482a893102781b97ec73ed2f0b
deepak reddy <deepakreddy6525@gmail.com>
2024-02-11 23:23:01 +0530

    initial commit

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,24 @@
+function fetchWeather() {
+    const city = document.getElementById('city').value.trim();
+    const apiKey = "63c9e53c66d238f7516bd2ba34463df3"; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                            <p>Temperature: ${(data.main.temp - 273.15).toFixed(2)} &#8451;</p>
+                            <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
+
+document.getElementById('weatherForm').addEventListener("submit",(event) => {
+    event.preventDefault();
+    fetchWeather();
+});
